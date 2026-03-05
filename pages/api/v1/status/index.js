8cab5fbd16024ad7430d0bc1e62c61bb211868af
Gustavo Pendeza <g.pendeza@hotmail.com>
2026-03-05 19:26:06 -0300

    chore: many changes on tests and migrations; removing biome; add and config prettier; and more...

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,20 +1,20 @@
-import database from 'infra/database.js'
+import database from 'infra/database.js';
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString()
+  const updatedAt = new Date().toISOString();
 
-  const dbVersion = await database.query('SHOW server_version;')
-  const dbVersionValue = dbVersion.rows[0].server_version
+  const dbVersion = await database.query('SHOW server_version;');
+  const dbVersionValue = dbVersion.rows[0].server_version;
 
-  const dbMaxConnections = await database.query('SHOW max_connections;')
-  const dbMaxConnectionsValue = dbMaxConnections.rows[0].max_connections
+  const dbMaxConnections = await database.query('SHOW max_connections;');
+  const dbMaxConnectionsValue = dbMaxConnections.rows[0].max_connections;
 
-  const dbName = process.env.POSTGRES_DB
+  const dbName = process.env.POSTGRES_DB;
   const dbOpenConnections = await database.query({
     text: 'SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;',
     values: [dbName]
-  })
-  const dbOpenConnectionsValue = dbOpenConnections.rows[0].count
+  });
+  const dbOpenConnectionsValue = dbOpenConnections.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
@@ -25,7 +25,7 @@ async function status(request, response) {
         open_connections: dbOpenConnectionsValue
       }
     }
-  })
+  });
 }
 
-export default status
+export default status;
